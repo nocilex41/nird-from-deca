63f232d165f5529c2d463f292fe7867c3bb9b38b
Alexis Veira <alexis@MacBook-Air-de-Alexis.local>
2025-12-04 20:18:16 +0100

    feat : ajout router vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            { index: true, element: <Home /> },
+            // { path: "profilage", element: <Profilage /> },
+            // { path: "instructions", element: <Instructions /> },
+            // { path: "produits", element: <Produits /> }
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
+);
